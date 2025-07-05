9771cddcba2f0f28969f308e08eaa2f0b493f44f
Kaky2727 <yeni.morales@duocuc.cl>
2025-07-05 01:09:09 -0400

    Add files via upload

---
+++ Github Menu Python.py
@@ -0,0 +1,62 @@
+
+def saludar(nombre): 
+    print("Hola", nombre)
+    print("aqui hay una calculadora simple creada con python")
+
+
+def sumar(): 
+    a=int(input("Ingrese el primer número: "))
+    b=int(input("Ingrese el segundo número: "))
+    resultado= a + b
+    print(resultado)
+
+
+def restar():
+    a=int(input("Ingrese el primer número: "))
+    b=int(input("Ingrese el segundo número: ")) 
+    resultado= a - b
+    print(resultado)
+ 
+
+def multiplicar():
+    a=int(input("Ingrese el primer número: "))
+    b=int(input("Ingrese el segundo número: ")) 
+    resultado= a * b
+    print(resultado)
+  
+
+def dividir():
+    a=int(input("Ingrese el primer número: "))
+    b=int(input("Ingrese el segundo número: ")) 
+    resultado= a / b
+    print(resultado)
+ 
+
+def menu(): 
+    print("1. Sumar")
+    print("2. Restar")
+    print("3. Multiplicar")
+    print("4. Dividir")
+    print("5. Salir")
+
+
+nombre=input("Ingrese su nombre: ")
+saludar(nombre)
+menu()
+opcion=int(input("Ingrese una opción: "))
+if opcion==1:
+    sumar()   
+elif opcion==2:
+    restar()    
+elif opcion==3:
+    multiplicar()   
+elif opcion==4:
+    dividir()    
+elif opcion==5:
+    print("Adiós")
+else:
+    print("Opción inválida")
+
+    print("Adiós")  
+
+    
